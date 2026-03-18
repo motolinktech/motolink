37fca9405b8944a0a6ccb37b452b0a9ae5857baf
John Vilela <johnvvilela@gmail.com>
2026-03-18 12:10:13 -0300

    chore: improve next config for production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false,
+  output: "standalone",
 };
 
 export default nextConfig;
